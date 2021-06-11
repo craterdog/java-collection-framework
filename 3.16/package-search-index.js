4742a76001bc98a07e50d8aa9d03becb36d08553
Derk Norton <derk.norton@gmail.com>
2021-06-11 10:03:41 -0600

    Added craterdog/java-collection-framework version 3.16 of the javadocs.

---
+++ 3.16/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"craterdog.collections"},{"l":"craterdog.collections.abstractions"},{"l":"craterdog.collections.primitives"}];updateSearchResults();
